78f9d143f634b875be731cfba0bec6e40a27639d
Andrew Yohanna <andrew.yohanna@corelogic.com.au>
2019-01-22 17:21:42 +1100

    App icons and launch screen logo

--- DouughUsersUITests/DouughUsersUITests.swift
+++ DouughUsersUITests/DouughUsersUITests.swift
@@ -26,9 +26,37 @@ class DouughUsersUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testBasicFlow() {
+        let app = XCUIApplication()
+        
+        // Testing users page
+        let usersTable = app.tables
+        XCTAssertEqual(usersTable.cells.count, 10)
+        let userCell1 = usersTable.cells.element(boundBy: 0)
+        XCTAssertTrue(userCell1.staticTexts["Barry White"].exists)
+        
+        // Navigating to user Details
+        userCell1.tap()
+        
+        // Testing user Details page
+        XCTAssertTrue(app.staticTexts["Barry White"].exists)
+        XCTAssertTrue(app.staticTexts["barry@white.com"].exists)
+        XCTAssertTrue(app.staticTexts["ID: 5"].exists)
+        
+        // Back to Main Page
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        
+        // Sort with Last Name
+        app.buttons["Last"].tap()
+        XCTAssertTrue(usersTable.cells.element(boundBy: 0).staticTexts["Benjamin Joseph Mark John Peter Simon Luke Chiong"].exists)
+        XCTAssertTrue(usersTable.cells.element(boundBy: 1).staticTexts["Joanne Galit"].exists)
+        XCTAssertTrue(usersTable.cells.element(boundBy: 2).staticTexts["Joe Joestar"].exists)
+        
+        
+        // Sort with ID
+        app.buttons["ID"].tap()
+        XCTAssertTrue(usersTable.cells.element(boundBy: 0).staticTexts["Bob Ong"].exists)
+        XCTAssertTrue(usersTable.cells.element(boundBy: 8).staticTexts["Edward Newgate"].exists)
+        XCTAssertTrue(usersTable.cells.element(boundBy: 9).staticTexts["Tina Loria"].exists)
     }
-
 }
